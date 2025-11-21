20a5231bde87beebf01a77c2115edcfc8f22bf07
Danielle-sn <stephanydanielle04@gmail.com>
2025-11-21 08:44:54 -0300

    código detect_obj.py atualizado

--- detect_obj.py
+++ detect_obj.py
@@ -1,30 +1,46 @@
-import cv2 
+import cv2
+import time
 from ultralytics import YOLO
 
-# carregar o modelo 
-model = YOLO('data/best3.pt')
+pTime = 0 # Variável para armazenar o tempo anterior (para cálculo de FPS)
 
-# inicializar câmera
+try:
+    model = YOLO('data/best4.pt')
+    #model.to('cuda') # usa GPU 
+except Exception as e:
+    print(f"Erro ao carregar o modelo YOLO: {e}")
+    exit()
+
+
 cam = cv2.VideoCapture(0)
+if not cam.isOpened():
+    print("Erro ao abrir a câmera. Verifique a conexão.")
+    exit()
 
-if cam.isOpened(): 
+while True:
     sucess, frame = cam.read()
+    if not sucess:
+        print("Falha ao capturar frame.")
+        break
 
-    while sucess: 
-        validacao, frame = cam.read()
+    # verbose=False para suprimir a saída do terminal a cada inferência
+    results = model.predict(frame, imgsz=800, half=True, verbose=False)
 
-        results = model(frame, imgsz=640, verbose=False)
+    annotated_frame = results[0].plot()
 
-        # Desenhar as caixas delimitadoras e rótulos no frame
-        annotated_frame = results[0].plot()
+    # Cálculo do FPS
+    cTime = time.time()
+    # O FPS = 1 / (diferença de tempo)
+    fps_real = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(annotated_frame, f'FPS: {int(fps_real)}', (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-        # Mostrar o frame anotado
-        cv2.imshow("YOLOv8 Detection", annotated_frame)
+    cv2.imshow("YOLOv8 Detection", annotated_frame)
 
-        key = cv2.waitKey(5) 
-        if key == ord('q'):
-            break 
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
 
 
-cam.release() 
-cv2.destroyAllWindows() 
+print("Gravação finalizada.")
+cam.release() # Libera a câmera
+cv2.destroyAllWindows()
